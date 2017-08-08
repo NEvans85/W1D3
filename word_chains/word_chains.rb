1dfb495e5faee65aa869b4195fa835715951df4d
App Academy Student <student@appacademy.io>
2017-08-08 11:56:37 -0700

    finish word chains

--- word_chains/word_chains.rb
+++ word_chains/word_chains.rb
@@ -1,17 +1,32 @@
 require 'set'
 
 class WordChainer
+
+  attr_reader :all_seen_words
   def initialize(dictionary_file_name)
     @dictionary = Set.new(File.readlines(dictionary_file_name).map(&:chomp))
   end
 
   def adjacent_words(target_word)
-    @dictionary.dup.keep_if do |word|
-      target_word.diff(word) == 1
-    end
+    @dictionary.dup.keep_if { |word| target_word.diff(word) == 1 }
   end
 
   def run(source, target)
+
+  end
+
+  def all_adjacent_words(target_word)
+    queue = adjacent_words(target_word).to_a
+    all_adjacent_words = Set.new
+    until queue.empty?
+      word = queue.shift
+      next_adj = adjacent_words(word)
+      next_adj.each do |word|
+        queue << word unless all_adjacent_words.include?(word)
+        all_adjacent_words << word
+      end
+    end
+    all_adjacent_words
   end
 end
 
